45d9345d0656ff31e019a32196c3253e6016d4f6
Quyetsama <you@example.com>
2021-11-09 20:06:35 +0700

    Bai4

--- App.js
+++ App.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const YourApp = () => {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{color: 'red'}}>
-        Quyết Sama! 🎉
-      </Text>
-      <Text>
-        Hello <Text style={{color: 'blue'}}>everyone</Text>
-      </Text>
-    </View>
-  );
+    return (
+        <View style={{ marginTop: 50 }}>
+            <Text style={ styles.text }>
+                Hello <Text style={{ color: '#e74c3c' }}>World</Text>
+            </Text>
+
+            <Text style={ styles.text }>
+                Hello{' '}
+                <Text style={ styles.text2 }>World</Text>
+            </Text>
+
+            <Text style={[ styles.text, { color: '#59595d' } ]}>
+                Hello{' '}
+                <Text style={{ color: '#fff', backgroundColor: '#59595d' }}>World</Text>
+            </Text>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    text: {
+        marginTop: 50,
+        textAlign: 'center',
+        fontSize: 30,
+        fontWeight: 'bold',
+        textTransform: 'uppercase'
+    },
+
+    text2: {
+        fontStyle: 'italic',
+        textDecorationLine: 'underline',
+        color: '#2980b9'
+    }
+})
 
 export default YourApp;
