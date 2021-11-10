f5c9cfaf7cd13cdbfcedc52e643e06d4aad00b80
Quyetsama <you@example.com>
2021-11-10 18:53:02 +0700

    Demension - Platform

--- App.js
+++ App.js
@@ -1,106 +1,51 @@
-import React, { useState } from 'react';
-import { SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View, Image, TouchableOpacityBase } from 'react-native';
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View, Image, Button, Platform } from 'react-native';
 import Constants from 'expo-constants';
 import { StatusBar } from 'expo-status-bar';
-import { Ionicons, FontAwesome5 } from '@expo/vector-icons';
 
-import facebookBanner from './assets/facebook-banner.jpg';
+import bulbOn from './assets/bulb-on.png';
+import bulbOff from './assets/bulb-off.png';
 
-const FaceBookLogin = () => {
+import { Dimensions } from 'react-native';
+
+
+const windowWidth = Dimensions.get('window').width
+const windowHeight = Dimensions.get('window').height
+const os = Platform.OS
+
+const TheLight = () => {
+    
     return (
-        <>
-            <Image source={ facebookBanner } style={{ width: '100%', height: null, aspectRatio: 1600/1002 }}/>
-            <SafeAreaView style={ styles.container }>
-                <View style={ styles.content }>
-                    <View>
-                        <TextInput style={ styles.input } placeholder='Số điện thoại hoặc email'/>
-                        <TextInput style={ styles.input } placeholder='Mật khẩu' secureTextEntry={ true }/>
+        <SafeAreaView style={ styles.container }>
+            <Image source={ bulbOn } style={ styles.image }/>
 
-                        <TouchableOpacity style={ styles.button }>
-                            <Text style={ styles.buttonText }>Đăng nhập</Text>
-                        </TouchableOpacity>
-
-                        <TouchableOpacity style={ styles.buttonAction }>
-                            <Text style={{ color: '#1693f2' }}>Quên mật khẩu?</Text>
-                        </TouchableOpacity>
-
-                        <TouchableOpacity style={ styles.buttonAction }>
-                            <Text style={{ color: '#1693f2' }}>Quay lại</Text>
-                        </TouchableOpacity>
-                    </View>
-                    
-                    <View>
-                        <View style={{ flexDirection: 'row', alignItems: 'center', alignSelf: 'center', width: '70%' }}>
-                            <View style={{ width: 30, flex: 1, height: 1, borderWidth: 0.5, borderColor: '#8f9ca6' }}></View>
-                            <Text style={{ width: 50, textAlign: 'center' }}>Hoặc</Text>
-                            <View style={{ width: 30, flex: 1, height: 1, borderWidth: 0.5, borderColor: '#8f9ca6' }}></View>
-                        </View>
-                        <TouchableOpacity style={[ styles.button, styles.buttonRegister ]}>
-                            <Text style={ styles.buttonTextRegister }>Tạo tài khoản mới</Text>
-                        </TouchableOpacity>
-                    </View>       
-                </View>
-            </SafeAreaView>
-        </>
+            <View style={{ flex:0.2, justifyContent: 'center', alignItems: 'center' }}>
+                <Button
+                    title={"Bật / tắt đèn " + os}
+                    color={ Platform.OS === "android" ? "#841584" : "blue"}
+                    accessibilityLabel="Learn more about this purple button"
+                />
+            </View>
+            
+        </SafeAreaView>
     )
 }
  
-const TEXT = {
-    color: '#fff',
-    textAlign: 'center'
-}
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff'
+        paddingTop: Constants.statusBarHeight
     },
 
-    content: {
+    image: {
         flex: 1,
-        marginTop: 20,
-        paddingHorizontal: 30,
-        justifyContent: 'space-between'
-    },
-
-    input: {
-        height: 50,
-        backgroundColor: '#fff',
-        borderRadius: 1,
-        borderColor: '#8f9ca6',
-        borderWidth: 0.2,
-        paddingHorizontal: 30, 
-        color: '#8f9ca6'
-    },
-
-    button: {
-        height: 50,
-        backgroundColor: '#1693f2',
-        borderRadius: 6,
-        marginTop: 15,
-        justifyContent: 'center'
-    }, 
-
-    buttonText: {
-        ...TEXT
-    }, 
-
-    buttonRegister: {
-        backgroundColor: '#dae9f5',
-        marginBottom: 30
-    },
-
-    buttonTextRegister: {
-        ...TEXT, 
-        color: '#1693f2'
-    },
-
-    buttonAction: {
-        marginTop: 10,
-        height: 30,
-        alignSelf: 'center',
-        justifyContent: 'center'
+        // maxWidth: '100%',
+        // maxHeight: '100%',
+        // resizeMode: 'contain',
+        backgroundColor: 'red',
+        width: windowWidth,
+        height: (windowWidth * 925) / 631
     }
 })
-  
-  export default FaceBookLogin
+
+export default TheLight
